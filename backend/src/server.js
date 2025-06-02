10a158936bff589bb5993f8defa0cda4384f4cb6
nitiankuldeep <nitiankuldeepsingh@gmail.com>
2025-06-02 18:48:18 +0530

    auth routes and recomdation /my friends

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import "dotenv/config";
+import authRoutes from './routes/auth.route.js';
+import userRoutes from "./routes/user.route.js";
+import connectDB from"./lib/db.js";
+import cookieParser from "cookie-parser"; 
+
+const app = express()
+ 
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+
+app.listen(PORT,()=>{
+    console.log(`serve is running at ${PORT}`);
+    connectDB();
+ })
